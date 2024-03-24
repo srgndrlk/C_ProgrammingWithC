bbd7dab71a2e829f74a5b82f1b0713d39cecbdc7
srgndrlk <srgndrlk@gmail.com>
2024-03-24 23:56:57 +0300

    snyc with pdf
    added naming convectşon for snyc with pdf files.

---
+++ Algo2/demo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+enum eAreas {programming=1,hardware,};
+
+int main(){
+
+enum eAreas area_variable;
+area_variable=hardware;
+printf("Area:%d\n",area_variable);
+area_variable+=2;
+printf("New Area :%d\n",area_variable);
+
+
+
+
+
+return 0;}
